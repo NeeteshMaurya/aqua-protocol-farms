dcebc99c22e2501b2574280a8a4b184641605f4e
NeeteshMaurya <neetesh010798@gmail.com>
2023-06-14 17:25:07 +0530

    responsive header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -17,16 +17,28 @@ const Header = () => {
 
 	
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<img style={{height:'40px',width:'40px'}} src={logo} alt="Logo" />
-			<p className="navbar-brand" style={{marginLeft:"25px"}} >AQUA PROTOCOL DEX</p>
-			<div style={{marginLeft:"1000px",borderRadius:"20px",borderColor:"black",borderWidth:"5px",}}>
-				{!account ? (
-					<UnlockButton width="100%" />
-				) : <p style={{fontWeight:'bold'}}>{account.slice(0,7)}</p>
-				}
-			</div>
-        </nav>  
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{width:'100%'}}>
+		<div className="container-fluid">
+		    <img style={{height:'40px',width:'40px'}} src={logo} alt="Logo" />
+		 	<a className="navbar-brand text-white" href="/" style={{marginLeft:"25px"}} >
+		 	   AQUA PROTOCOL DEX
+	 	    </a>
+		   
+		   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		   <span className="navbar-toggler-icon" />
+		   </button>
+		   <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+			  <ul className="navbar-nav ">
+					<li className="nav-item " >
+						{!account ? (
+							<UnlockButton width="100%" />
+						) : <p style={{fontWeight:'bold',borderRadius:'24px',padding:'11px'}} className="text-white border border-primary">{account.slice(0,7).concat('...')}</p>
+						}	
+					</li>
+			  </ul>
+		   </div>
+		</div>
+	 </nav>
 	);
 };
 
